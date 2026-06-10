42d488e8cd8b249e2ec48d2fdc229956e673a067
hamzahmurad23-blip <hamzahmurad23@gmail.com>
2026-06-10 09:29:45 +0000

    Add files via upload

---
+++ Hamzah2.java
@@ -0,0 +1,16 @@
+public class Hamzah2 {
+    public static void main(String[] args){
+        System.out.println("          T       ");
+        System.out.println("         TTT      ");
+        System.out.println("        TTTTT     ");
+        System.out.println("       TTTTTTT    ");
+        System.out.println("      TTTTTTTTT   ");
+        System.out.println("    TTTTTTTTTTTT  ");
+        System.out.println("   TTTTTTTTTTTTTT ");
+    }
+
+
+
+}
+
+
